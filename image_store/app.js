a8864146c4878f26080bb7f274e86cc3c908f75d
Manikanta Rahul <manikantarahul.n@gmail.com>
2019-04-23 17:22:41 +0530

    Update app.js

--- image_store/app.js
+++ image_store/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
