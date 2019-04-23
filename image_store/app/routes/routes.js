42a34929c6867f755f4fd151f590275406563d67
Manikanta Rahul <manikantarahul.n@gmail.com>
2019-04-23 18:37:12 +0530

    Update routes.js

--- image_store/app/routes/routes.js
+++ image_store/app/routes/routes.js
@@ -1,4 +1,4 @@
 module.exports=(app)=>{
     const control_fun=require('../controllers/controllers.js');
     app.post('/', control_fun.post_image);
-}
+    app.post('/base64', control_fun.base64);}
